6d6ddaf2ff966894abf29f0dc2837178469608c7
Eli Porter <eli.the.game.player@gmail.com>
2022-04-07 20:09:09 -0700

    initial commit

---
+++ cutepockets.com/p5forcutepockets/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(outerWidth, outerHeight*2);
+}
+
+function draw() {
+  background(1);
+  fill('lightBlue')
+
+  textSize(80)
+  textAlign(CENTER)
+  text("Hello, and welcome to CutePockets!", width/2, 80)
+  textSize(50)
+  text('Here, we will')
+
+}
